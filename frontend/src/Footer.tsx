a07554a32f9bc48037819cd77ab6ec5c48371bce
Martin <martin.louvard@gmail.com>
2023-06-29 03:22:51 +0200

    Added Three.js and NestJS SVGs to the footer

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import nestLogo from '/nest.svg'
+import threeLogo from '/three.svg'
 
 interface FooterProps {
   // Define any props you need for the Footer
@@ -10,11 +12,17 @@ const Footer: React.FC<FooterProps> = () => {
   return (
     <div id='stack-logos'>
         <label id='stack-logos-label'>Made With</label>
+        <a href="https://nestjs.com/" target="_blank">
+          <img src={nestLogo} className="logo" alt="NestJs logo" />
+        </a>
+        <a href="https://threejs.org/" target="_blank">
+          <img src={threeLogo} className="logo" alt="Three.js logo" />
+        </a>
         <a href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
     </div>
   );
