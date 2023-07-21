0956103e899d85416b44b1750c6012e8d81712e4
Martin <martin.louvard@gmail.com>
2023-07-21 20:43:09 +0200

    added authentication system in backend with jwt implementation. Plugged signup and login components in frontend to store accesstoken in redux

--- frontend/src/components/Authentication/LoginForm.tsx
+++ frontend/src/components/Authentication/LoginForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Form from './Form';
-import { setUser } from './userReducer'
+import  login  from './login.ts'
+import { setUser } from './userReducer';
 import { useAppDispatch } from '../../hooks';
 
 const LoginForm: React.FC = () => {
@@ -8,31 +9,6 @@ const LoginForm: React.FC = () => {
   const [password, setPassword] = useState('');
   const dispatch = useAppDispatch()
 
-  const login = async () =>{
-
-    const requestOptions = {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-    };
-
-    try{
-      await fetch(`http://localhost:3001/users/${username}`, requestOptions)
-      .then(response => response.json())
-      .then(data =>{
-        console.log(data)
-        if (data.password == password){
-          const email = data.email
-          const isLoggedIn = true
-          dispatch(setUser({email, username, password, isLoggedIn}))
-        }
-        else
-          alert("Wrong password");
-      });
-    }catch(err) {
-      alert(err);
-    }
-
-  }
   
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
    event.target.id === "username" ? setUsername(event.target.value): 
@@ -40,9 +16,12 @@ const LoginForm: React.FC = () => {
     ()=>{}
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    username.length && password.length && login()
+    username.length && password.length 
+    const user = await login(username,password)
+    const isLoggedIn = true
+    dispatch(setUser({user, isLoggedIn}))
   };
 
   return (
