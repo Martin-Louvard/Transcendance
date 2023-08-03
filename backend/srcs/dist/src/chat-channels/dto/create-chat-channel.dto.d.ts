002fa5b13f85c87d54003c0b0b448ee5bee9f141
Martin <martin.louvard@gmail.com>
2023-08-03 18:55:01 +0200

    deleted node modules and dist

--- backend/srcs/dist/src/chat-channels/dto/create-chat-channel.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { User } from 'src/users/entities/user.entity';
-export declare class CreateChatChannelDto {
-    owner: User;
-    password: string;
-    channelType: string;
-    name: string;
-    participants: User;
-}
--- backend/srcs/dist/src/chat-messages/entities/chat-message.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ChatMessage = void 0;
-class ChatMessage {
-}
-exports.ChatMessage = ChatMessage;
-//# sourceMappingURL=chat-message.entity.js.map
