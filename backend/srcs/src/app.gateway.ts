2b3b37594a34fb07b7fffaed567608bb8256755b
Martin <martin.louvard@gmail.com>
2023-08-10 20:04:15 +0200

    User socket at top level in front. The gateway now keep tracks of connected users

--- backend/srcs/src/app.gateway.ts
+++ backend/srcs/src/app.gateway.ts
@@ -1,12 +1,34 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @WebSocketGateway({ cors: '*'})
-export class AppGateway {
+export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private prisma: PrismaService){}
-
   @WebSocketServer()
   server;
+
+  connected_users_id: Set<number> = new Set();
+
+  async handleConnection(client: any) {
+    // A client has connected
+    const newly_connected_user_id = client.handshake.headers.user_id
+    if(newly_connected_user_id)
+      this.connected_users_id.add(newly_connected_user_id);
+
+      // Notify connected clients of current users
+    this.server.emit('connected_users', Array.from(this.connected_users_id));
+  }
+
+  async handleDisconnect(client: any) {
+    // A client disconnects
+    const disconnecting_user_id = client.handshake.headers.user_id
+    if(disconnecting_user_id)
+      this.connected_users_id.delete(disconnecting_user_id);
+    
+      // Notify connected clients of current users
+    this.server.emit('connected_users', Array.from(this.connected_users_id));
+  }
+
   @SubscribeMessage('message')
   async handleMessage(@MessageBody() body: Array<any>): Promise<void> {
     const message = await this.prisma.chatMessage.create({data: {channelId: body[0], senderId: body[1] ,content: body[2]}})
--- frontend/srcs/src/components/Dashboard/index.tsx
+++ frontend/srcs/src/components/Dashboard/index.tsx
@@ -3,19 +3,36 @@ import LeftMenu from './LeftMenu';
 import './Dashboard.css'
 import Experience from '../../Experience';
 import { Canvas } from '@react-three/fiber';
+import io, { Socket } from "socket.io-client";
+import { useAppSelector } from '../../hooks';
 
 const Dashboard: React.FC = () => {
   const [isPlaying , setIsPlaying ] = useState(false)
   const [fullscreen, setFullScreen] = useState(false)
+  const [connectedUsersId, setConnectedUsersId] = useState([0])
+  const [socket, setSocket] = useState<Socket>()
+  const user = useAppSelector((state)=> state.user)
 
-  
+  useEffect(()=>{
+    const newSocket= io("http://localhost:3001/",{extraHeaders: {user_id: user.id.toString()}});
+    setSocket(newSocket)
+  },[])
+
+  const connectedUsersListener = (connected_users_id: Array<number>) =>{
+    setConnectedUsersId(connected_users_id)
+  }
+  useEffect(()=>{
+    socket?.on("connected_users", connectedUsersListener)
+    return () => {socket?.off("connected_users", connectedUsersListener)}
+  },[connectedUsersListener])
+
+
   useEffect(()=>{}, [fullscreen])
 
   return (<>
     <div className="dashboard-wrapper">
       <LeftMenu hideMenu={fullscreen}/>
       <div className="canvas-wrapper">
-
         {isPlaying ?
         <>
         <img id={"back"} onClick={() =>{ setFullScreen(!fullscreen);}} className='fullscreen-button' src={'fullscreen.svg/'} />
@@ -25,6 +42,7 @@ const Dashboard: React.FC = () => {
           </Canvas>
         </> : 
           <div>
+            <p>Number of connected users: {connectedUsersId.length}</p>
             <button className="play-button" onClick={()=>{setIsPlaying(true);}}>Play</button>
           </div>}
       </div>
