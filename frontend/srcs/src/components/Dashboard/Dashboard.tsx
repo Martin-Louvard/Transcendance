f55aa73247cdc15b6a39009c7f38a3cb6692144e
Martin <martin.louvard@gmail.com>
2023-08-11 16:25:35 +0200

    new menu css

--- frontend/srcs/src/components/Dashboard/Dashboard.tsx
+++ frontend/srcs/src/components/Dashboard/Dashboard.tsx
@@ -6,26 +6,9 @@ import { Canvas } from '@react-three/fiber';
 
 const Dashboard: React.FC = () => {
   const [isPlaying , setIsPlaying ] = useState(false)
-  const [fullscreen, setFullScreen] = useState(false)
-  const [menuCss, setmenuCss] = useState("open-menu")
 
-  const toggleMenu = () =>{
-    if (menuCss.charAt(0) == 'o')
-      setmenuCss("close-menu menu-transition-close")
-    else
-      setmenuCss("open-menu menu-transition-open")
-    setFullScreen(!fullscreen);
-  }
-  
-  useEffect(()=>{}, [fullscreen])
 
-  return (<>
-    
-      <div className={`dashboard-wrapper ${menuCss}`}>
-        <img className={`logo-nav menu-icon`} src={'/menu.svg'} onClick={toggleMenu}/>
-        <LeftMenu hideMenu={fullscreen}/>
-      </div>
-
+/*
       <div className="canvas-wrapper">
         {isPlaying ?
           <>
@@ -38,6 +21,13 @@ const Dashboard: React.FC = () => {
             <button className="play-button" onClick={()=>{setIsPlaying(true);}}>Play</button>
           </div>
           }
+      </div>
+*/
+
+  return (<>
+    
+      <div className={`dashboard-wrapper`}>
+        <LeftMenu />
       </div>
 
   </>
