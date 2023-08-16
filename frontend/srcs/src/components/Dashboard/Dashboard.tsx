3c47c0bea3fef39951c38af307d5ab735c111c87
Martin <martin.louvard@gmail.com>
2023-08-16 20:01:39 +0200

    refactored Dashboard

--- frontend/srcs/src/components/Dashboard/Dashboard.tsx
+++ frontend/srcs/src/components/Dashboard/Dashboard.tsx
@@ -1,31 +1,30 @@
 import React, { useRef, useEffect } from 'react';
 import LeftMenu from '../Menu/LeftMenu';
-import './Dashboard.scss'
+import './Dashboard.scss';
 import { Lobby } from '../Game/Lobby';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 
 const Dashboard: React.FC = () => {
-  const user = useAppSelector((state)=>state.session.user)
+  const user = useAppSelector((state) => state.session.user);
   const dispatch = useAppDispatch();
-  const isInitialLoadRef = useRef(true); 
+  const isInitialLoadRef = useRef(true);
 
   useEffect(() => {
-    // Dispatch WebSocket actions
-    if (isInitialLoadRef.current){
-      isInitialLoadRef.current = false
-      dispatch({ type: 'WEBSOCKET_CONNECT', payload: user?.id });
+    if (isInitialLoadRef.current) {
+      isInitialLoadRef.current = false;
+      if (user) {
+        dispatch({ type: 'WEBSOCKET_CONNECT', payload: user.id });
+      }
     }
-  }, [dispatch]);
+  }, [dispatch, user]);
 
-  return (<>
-
-      <div className={`dashboard-wrapper`}>
-        <LeftMenu />
-        <div className="canvas-wrapper">
-          <Lobby/>
-        </div>
+  return (
+    <div className="dashboard-wrapper">
+      <LeftMenu />
+      <div className="canvas-wrapper">
+        <Lobby />
       </div>
-  </>
+    </div>
   );
 };
 
