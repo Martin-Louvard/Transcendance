b63afcef6ba9e5aa5cfab5026a8ce9b8e6808c26
Youkass <youribougre@gmail.com>
2023-09-28 16:17:31 +0200

    [FIX] blocked users

--- frontend/srcs/src/components/Chat/PopupChatManagement/blockUserButton.tsx
+++ frontend/srcs/src/components/Chat/PopupChatManagement/blockUserButton.tsx
@@ -1,13 +1,20 @@
 import { User, ChatChannels } from "../../../Types.ts";
 import { useAppSelector, useAppDispatch } from "../../../redux/hooks";
 import { BiBlock } from "react-icons/bi";
+import { toast } from "react-hot-toast";
 
 const BlockUserButton = ({ user } : {user: User}) => {
   const currentUser = useAppSelector((state) => state.session.user);
   const dispatch = useAppDispatch();
+  const friendship = useAppSelector(
+    (state) => state.session.friendships)?.filter(
+      (relations) => relations.friend_id === user.id || relations.friend_id === currentUser?.id)[0];
 
-  const handleBlockUser = () => {
-      dispatch({type: 'BLOCK_USER', payload:[user.id, currentUser?.id]});
+  const handleBlockUser = async () => {
+    dispatch({
+      type: "WEBSOCKET_SEND_FRIEND_REQUEST",
+      payload: [friendship?.id, user.username, 'BLOCKED']})
+    toast.success("User Blocked")
   }
   return (
     <div className="management-button" onClick={() => handleBlockUser()}>
